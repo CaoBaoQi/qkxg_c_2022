5484ffa605bf5d6aaadfd79020f9e23e6b294575
曹宝琪 <2023cbq@gmail.com>
2024-01-08 12:21:16 +0800

    init commit

---
+++ src/basic/basic01.c
@@ -0,0 +1,10 @@
+//
+// Created by caobaoqi on 2024/1/8.
+//
+
+#include <stdio.h>
+
+int main() {
+    printf("基础部分-01\n");
+    return 0;
+}
---
+++ src/data-structure/data01.c
@@ -0,0 +1,10 @@
+//
+// Created by caobaoqi on 2024/1/8.
+//
+
+#include <stdio.h>
+
+int main() {
+    printf("数据结构-01\n");
+    return 0;
+}
