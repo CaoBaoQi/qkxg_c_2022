a386624e025e11dc5c589a5e4e6d77fd38890bc7
曹宝琪 <2023cbq@gmail.com>
2024-01-08 13:42:56 +0800

    data: 数据结构-01

--- src/data-structure/data01.c
+++ src/data-structure/data01.c
@@ -1,10 +1,29 @@
-//
-// Created by caobaoqi on 2024/1/8.
-//
+void data01_demo01();
 
-#include <stdio.h>
+void data01_demo02();
 
+#include "../common/cbq.h"
+
+/**
+ * @author caobaoqi
+ * @name 数据结构-01
+ * @date 2024/1/8 13:40
+ */
 int main() {
-    printf("数据结构-01\n");
+    data01_demo01();
+    data01_demo02();
     return 0;
+}
+
+void data01_demo01() {
+    printHeader("data01_demo01");
+
+    printFooter("data01_demo01");
+
+}
+
+void data01_demo02() {
+    printHeader("data01_demo02");
+
+    printFooter("data01_demo02");
 }
